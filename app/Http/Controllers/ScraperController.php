ef30ad6726b2083438b1e00848ef4f6a7a781fab
Satheesh Narayanan <nsatheesh87@gmail.com>
2017-10-14 04:58:52 +0000

    Initial lumen setup - Ready to go

---
+++ app/Http/Controllers/ScraperController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request as Request;
+
+class ScraperController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function show($id = '', Request $request)
+    {
+
+    }
+
+    public function create(Request $request)
+    {
+
+    }
+
+    //
+}
