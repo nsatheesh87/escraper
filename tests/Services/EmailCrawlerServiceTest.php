06c60b238e693646383b6e9108c3eda40892b62c
Satheesh Narayanan <nsatheesh87@gmail.com>
2017-10-17 12:58:23 +0000

    simple test case added from email crawler class unit testing

---
+++ tests/Services/EmailCrawlerServiceTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Services;
+
+use App\Scraper\Services\EmailScraper;
+
+class EmailCrawlerServiceTest extends WebTestCase
+{
+    /**
+     *
+     */
+    public function testCrawler() {
+        $url = 'https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_link_mailto';
+        $output = ['someone@example.com', 'someone@example.com']
+        $emailScraper = new EmailScraper($url);
+        $this->assertEquals($output, $emailScraper);
+    }
+}
